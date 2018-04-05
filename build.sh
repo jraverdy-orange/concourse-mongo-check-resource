c201171a016aba345d1560482a6f4ffee5d94bfc
Jérémy Raverdy <jeremy.raverdy@orange.com>
2018-04-05 15:51:05 +0200

    bad building settings

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 export VERSION=1.0.0
 
-echo Building https://github.com/jraverdy-orange/concourse-mongo-check-resource:${VERSION}
+echo Building concourse-mongo-check-resource:${VERSION}
 
-docker build --no-cache -t https://github.com/jraverdy-orange/concourse-mongo-check-resource:${VERSION} . --build-arg http_proxy=http://10.171.41.73:3128
+docker build --no-cache -t concourse-mongo-check-resource:${VERSION} . --build-arg http_proxy=http://10.171.41.73:3128
 
 echo Pushing last version on docker-hub
-docker tag https://github.com/jraverdy-orange/concourse-mongo-check-resource:${VERSION} jraverdyorange/https://github.com/jraverdy-orange/concourse-mongo-check-resource:${VERSION}
-docker tag jraverdyorange/https://github.com/jraverdy-orange/concourse-mongo-check-resource:${VERSION} jraverdyorange/https://github.com/jraverdy-orange/concourse-mongo-check-resource:latest
-docker push jraverdyorange/https://github.com/jraverdy-orange/concourse-mongo-check-resource
+docker tag concourse-mongo-check-resource:${VERSION} jraverdyorange/concourse-mongo-check-resource:${VERSION}
+docker tag jraverdyorange/concourse-mongo-check-resource:${VERSION} jraverdyorange/concourse-mongo-check-resource:latest
+docker push jraverdyorange/concourse-mongo-check-resource
