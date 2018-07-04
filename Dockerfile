a17fedc1fa3aae770a7ffed649c3315f2dfcd65d
Jérémy RAVERDY <jeremy.raverdy@orange.com>
2018-07-04 13:42:13 +0200

    Fetching python dependencies

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM alpine:edge
-RUN  apk add --update perl perl-canary-stability perl-json curl
+RUN  apk add --update perl perl-canary-stability perl-json curl python py-pip gcc
 COPY /assets /opt/resource
--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-export VERSION=1.0.0
+export VERSION=1.1.0
 
 echo Building concourse-mongo-check-resource:${VERSION}
 
-docker build --no-cache -t concourse-mongo-check-resource:${VERSION} . --build-arg http_proxy=http://10.171.41.73:3128
+docker build --no-cache -t concourse-mongo-check-resource:${VERSION} . \
+		--build-arg http_proxy=http://10.171.41.73:3128 \
+		--build-arg HTTP_PROXY=http://10.171.41.73:3128
+
 
 echo Pushing last version on docker-hub
 docker tag concourse-mongo-check-resource:${VERSION} jraverdyorange/concourse-mongo-check-resource:${VERSION}
